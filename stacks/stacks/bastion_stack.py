ea33debff15aeaa054f628c8ab9d5f10026303ec
0ni0nrings <cloud_user@c7d6e528391c.mylabserver.com>
2022-03-08 02:42:54 +0000

    added bastion stack

--- stacks/stacks/bastion_stack.py
+++ stacks/stacks/bastion_stack.py
@@ -5,12 +5,18 @@ from aws_cdk import (
     aws_ssm as ssm,
 )
 from constructs import Construct
+from cdk_ec2_key_pair import KeyPair
 
 class BastionStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str,vpc: ec2.Vpc, sg: ec2.SecurityGroup, **kwargs) -> None: # vpc & sg are newly added paramaters
         super().__init__(scope, construct_id, **kwargs)
-
+        ### https://github.com/aws/aws-cdk/issues/5252 ###
+        # bastion_key = KeyPair(self, 'bastion-key',
+        #     name = 'bastion-key',
+        #     description = 'keypair for bastion host',
+        #     storePublicKey = True
+        # ),
         bastion_host = ec2.Instance(self, 'bastion-host',
             instance_type=ec2.InstanceType(
                 instance_type_identifier='t2.micro'
@@ -22,8 +28,8 @@ class BastionStack(Stack):
                 storage = ec2.AmazonLinuxStorage.GENERAL_PURPOSE
             ),
             vpc=vpc,
-            key_name='devops',
-            vpc_subnets=ec2.SubnetSelection(
+            key_name = 'devops', # create this ec2 key pair manually in AWS console
+            vpc_subnets = ec2.SubnetSelection(
                 subnet_type = ec2.SubnetType.PUBLIC
             ),
             security_group=sg
